3f05526e9151c0e77030b58a8e9ecab6bdffd0eb
a-sambhab <saisambhav21chaini@gmail.com>
2021-09-10 15:20:43 +0530

    minor edits

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,14 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "static/favicon.ico", // This path is relative to the root of the site.
+        icons: [
+          {
+            "src": "https://res.cloudinary.com/dzbdnlr0f/image/upload/v1631218435/alcove/alcove_white_logo_a8hxhg.png",
+            "sizes": "196x196",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ], // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
